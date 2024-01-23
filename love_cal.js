6ab9882ec7b05d996dc106ff244920cb52d2d0cd
KHALED MAHMUD SUJON <86775777+sujonkhaled@users.noreply.github.com>
2024-01-23 17:57:13 +0800

    Add files via upload
    added

---
+++ love_cal.js
@@ -0,0 +1,25 @@
+prompt("what is your name ");
+prompt("what is their name" );
+
+var lovescore = Math.random()*100;
+lovescore = Math.floor(lovescore) + 1;
+
+if(lovescore > 100){
+
+alert("your lovescore is " +lovescore + "%" +"you love each other like laili mojnu");
+
+    
+}
+
+    if(lovescore>30 && lovescore<=70){
+           alert("your lovescore is " +lovescore + "%"); 
+}
+
+
+    
+if (lovescore<=30){
+   alert("your lovescore is " +lovescore + "%" + "you go together like oil and water "); 
+}
+
+
+ 
